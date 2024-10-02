3d7936c57abd42d7704c718bb53370065f7c76be
Ruan Batista Rodrigues <145821191+zzruanxx@users.noreply.github.com>
2024-10-02 01:18:58 -0300

    listasdelistas

---
+++ listas.py
@@ -0,0 +1,12 @@
+times_nfl = ["Dallas Cowboys", "Green Bay Packers", "New England Patriots", "San Francisco 49ers"]
+print(times_nfl)
+
+info_times_nfl = [
+    ["Dallas Cowboys", "NFC East", 5],
+    ["Green Bay Packers", "NFC North", 4],
+    ["New England Patrios", "AFC East", 6],
+    ["San Francisco 49ers", "NFC West", 5]
+    
+    ]
+    
+print(info_times_nfl)
